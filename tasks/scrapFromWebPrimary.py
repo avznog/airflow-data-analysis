8089b4151e6e440b3e793b785fd8b5a5cf75c8f9
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-06-07 16:57:10 +0200

    ✅ update rows name

--- tasks/scrapFromWebPrimary.py
+++ tasks/scrapFromWebPrimary.py
@@ -3,8 +3,7 @@ from airflow.models import Variable
 
 def scrapFromWebPrimary (ti): 
 	# ? url to scrap
-	print(Variable.get("count_rows_request_primary"))
-	url = f'https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&q=&lang=fr&rows={Variable.get("count_rows_request_primary")}&facet=departement&facet=region&facet=Gazole_maj&facet=Gazole_prix'
+	url = f'https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&q=&lang=fr&rows={Variable.get("rows")}&facet=departement&facet=region&facet=Gazole_maj&facet=Gazole_prix'
 
 	# ? getting data from api.gouv.fr
 	response = requests.get(url)
--- tasks/scrapFromWebSecond.py
+++ tasks/scrapFromWebSecond.py
@@ -3,8 +3,7 @@ from airflow.models import Variable
 
 def scrapFromWebSecond (ti): 
 	# ? url to scrap
-	print(Variable.get("count_rows_request_second"))
-	url = f'https://odre.opendatasoft.com/api/records/1.0/search/?dataset=conso-epci-annuelle&q=&rows={Variable.get("count_rows_request_second")}&facet=annee&facet=libelle_epci&facet=libelle_departements&facet=libelle_regions&facet=e_operateurs&facet=g_operateurs'
+	url = f'https://odre.opendatasoft.com/api/records/1.0/search/?dataset=conso-epci-annuelle&q=&rows={Variable.get("rows")}&facet=annee&facet=libelle_epci&facet=libelle_departements&facet=libelle_regions&facet=e_operateurs&facet=g_operateurs'
 
 	# ? getting data from api.gouv.fr
 	response = requests.get(url)
